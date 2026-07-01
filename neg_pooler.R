db2f06a090228b3ece3068806075fcc554a69845
Tuyen Huynh <hntuyen282@gmail.com>
2026-07-01 11:48:42 +0700

    pool negative samples into batches

--- neg_pooler.R
+++ neg_pooler.R
@@ -1,10 +1,14 @@
 library(tidyverse)
+library(purrr)
 
 neg_samples <- read_csv("selected_neg_samples-ansi.csv") %>%
   filter_out(year == 2018)
 
 neg_samples %>%
   mutate(
-    pool = sprintf("76DX-152-5%03d", rep(1:175, 4))
+    pool = sprintf(
+      "76DX-152-5%03d",
+      map(seq.int(1, 175, 1), ~ rep(.x, 4)) %>% unlist()
+    )
   ) %>%
   write_excel_csv("selected_neg_samples-ansi-pooled.csv")
